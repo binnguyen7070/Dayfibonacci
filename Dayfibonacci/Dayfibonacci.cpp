213abe82a10c2cc48174e86bbc5ea6570af44f1c
SPC <SPC@29-LAB402>
2024-07-08 19:13:30 +0700

    Add project files.

---
+++ Dayfibonacci/Dayfibonacci.cpp
@@ -0,0 +1,25 @@
+// Dayfibonacci.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Day Fibonacci!!!\n";
+	int x1 = 1, x2 = 1;
+	for (int i = 0; i < 20; i++)
+	{
+		cout << x1 << "|";
+		int tong = x1 + x2;
+		x1 = x2;
+		x2 = tong;
+	}
+}
+
+
+
+
+
+
+
+
+
